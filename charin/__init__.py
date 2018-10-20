cc3ee157828d115c948a5f660cd723e9ba5351ad
Tetsuya Mishima <tmtm.yagi.007@gmail.com>
2018-10-20 21:31:54 +0900

    templatespathの設定

--- charin/__init__.py
+++ charin/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
+from jinja2 import FileSystemLoader
 
 app = Flask(__name__)
+app.jinja_loader = FileSystemLoader('./templates')
 app.config.from_object('charin.config')
 
 db = SQLAlchemy(app)
