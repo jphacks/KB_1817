7e13b598e6a6634a9c52cb8e38babbdd120b3d2f
Tetsuya Mishima <tmtm.yagi.007@gmail.com>
2018-10-21 00:50:21 +0900

    heroku関連

--- charin/config.py
+++ charin/config.py
@@ -1,9 +1,10 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/charin?charset=utf8'.format(**{
-        'user': os.getenv('DB_USER', 'root'),
-        'password': os.getenv('DB_PASSWORD', ''),
-        'host': os.getenv('DB_HOST', 'localhost'),
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{dbname}?charset=utf8'.format(**{
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'host': os.getenv('DB_HOST'),
+        'dbname': os.getenv('DB_DATABASE')
     })
 SECRET_KEY = 'secret key'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
