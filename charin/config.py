772519af5be4099e4e41a6b802ef054ccfa06011
Tetsuya Mishima <tmtm.yagi.007@gmail.com>
2018-10-21 02:52:19 +0900

    dbconfig

--- charin/config.py
+++ charin/config.py
@@ -1,10 +1,10 @@
 import os
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{dbname}?charset=utf8'.format(**{
-        'user': os.getenv('DB_USERNAME'),
-        'password': os.getenv('DB_PASSWORD'),
-        'host': os.getenv('DB_HOST'),
-        'dbname': os.getenv('DB_DATABASE')
+        'user': os.getenv('DB_USERNAME',"root"),
+        'password': os.getenv('DB_PASSWORD',""),
+        'host': os.getenv('DB_HOST','localhost'),
+        'dbname': os.getenv('DB_DATABASE','charin')
     })
 SECRET_KEY = 'secret key'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
