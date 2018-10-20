221137e421fcd1747d2d6e2e0a5e312d3486eccc
瀬尾浩史 <matatabi1145@gmail.com>
2018-10-20 12:36:18 +0900

    位置情報とってくるjs

---
+++ static/js/location.js
@@ -0,0 +1,29 @@
+//GPS取得
+var num = 0;
+var watch_id;
+
+window.onload = function(){
+    watch_id = navigator.geolocation.watchPosition(test2, function(e) { alert(e.message); }, {"enableHighAccuracy": true, "timeout": 50000, "maximumAge": 5000});
+}
+
+//開いてから位置情報を得るたびにこれが動く
+function test2(position) {
+
+    var geo_text = "緯度:" + position.coords.latitude + "\n";
+    geo_text += "経度:" + position.coords.longitude + "\n";
+    geo_text += "高度:" + position.coords.altitude + "\n";
+    geo_text += "取得時刻:" + date + "\n";
+
+    document.getElementById('position_view').innerHTML = geo_text;
+
+}
+
+//サーバーと非同期通信データ受け取り
+var xhr= new XMLHttpRequest();
+xhr.open("GET","/index.html",true);
+xhr.send();
+
+//データ送る
+xhr.open('POST', '/index.html',true);
+xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+xhr.send( 'latitude=position.coords.latitude&longitude=position.coords.longitude&altitude=position.coords.altitude&date=date' );
