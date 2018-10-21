c59d2ad8e20cd48bc79264b69a846abbeb009647
Tetsuya Mishima <tmtm.yagi.007@gmail.com>
2018-10-21 13:38:14 +0900

    受け取った通知が出る

--- charin/static/js/location.js
+++ charin/static/js/location.js
@@ -28,8 +28,8 @@ function locate(position) {
     xhr_post.onreadystatechange = function() {
         if (xhr_post.readyState === 4) {
             var credit = parseInt(xhr_post.response);
-            var now_credit = parseInt(document.getElementsByName("credit"))
-            console.log(credit,now_credit)
+            var now_credit = parseInt(document.getElementsByName("credit")[0].value)
+            console.log(credit, now_credit)
             if (now_credit < credit ) {
                 alert("あなたに誰かがCharinしました！")
             }
